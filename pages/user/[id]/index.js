1dca8cef7ce398c2d55b01741bc4866049f173f7
menimesh <nimeshkhatiwadaa@gmail.com>
2024-08-13 16:27:17 +0545

    useSwr

--- pages/user/[id]/index.js
+++ pages/user/[id]/index.js
@@ -1,22 +1,64 @@
 import { useState,useEffect } from "react";
 import { useRouter } from "next/router";
 
+
+import useSWR from "swr";
+const fetcher = (...args) => fetch(...args).then(res=>res.json())
+
 const UserInfoPage = ()=>{
     const router = useRouter();
-    const [userInfo,setUserInfo]=useState(null);
-    console.log(userInfo);
-    useEffect(()=>{
-        const id=router.query.id;
-        async function getUserById(id){
-            const data = await fetch(`https://dummyjson.com/users/${id}`);
-            setUserInfo(await data.json());
-        }
-        getUserById(id);
-    },[router.query.id])
-    return(
-        <div>
-         {userInfo?.firstName} {userInfo?.lastName} {userInfo?.age}
-        </div>
-    )
-}
+    const id=router.query.id;
+const {data,error}=useSWR(`https://dummyjson.com/users/${id}`,fetcher);
+if (error) return <div>Failed to load user data.</div>;
+if (!data) return <div>Loading...</div>;
+console.log(data);
+return (
+    <>
+    <div class="container">
+  <div >
+  <h2 class="text-center">User Information.</h2>
+  </div>
+</div>
+    <table class="table">
+  <thead class="thead-green">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Gender</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">{data.id}</th>
+      <td>{data.firstName}</td>
+      <td>{data.lastName}</td>
+      <td>{data.email}</td>
+      <td>{data.gender}</td>
+    </tr>
+   
+  </tbody>
+</table>
+       
+    </>
+);
+};
 export default UserInfoPage;
+
+
+
+   // const [userInfo,setUserInfo]=useState(null);
+    // console.log(userInfo);
+    // useEffect(()=>{
+    //     async function getUserById(id){
+    //         const data = await fetch(`https://dummyjson.com/users/${id}`);
+    //         setUserInfo(await data.json());
+    //     }
+    //     getUserById(id);
+    // },[router.query.id])
+    // return(
+    //     <div>
+    //      {userInfo?.firstName} {userInfo?.lastName} {userInfo?.age}
+    //     </div>
+    // )
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,70 +1,53 @@
 "use client";  // Ensure it's treated as a Client Component
 
-// import React, { useState } from 'react';
-// import { useRouter } from 'next/router';
-
-// const Index = () => {
-//   const router = useRouter();
-//   const [value, setValue] = useState('');
-
-//   return (
-//     <>
-//       <div>This is the index file</div>
-      
-//       <input 
-//         type='text' 
-//         onChange={(e) => setValue(e.target.value)}
-//         placeholder="Enter a value"
-//       />
-      
-//       <button onClick={() => {
-//         if (value.trim() !== "") {
-//           router.push({
-//             pathname: '/user/[test]/inform',
-//             query: { test: value }
-//           });
-//         } else {
-//           router.replace("/");
-//         }
-//       }}>
-//         Click
-//       </button>
-      
-//       <button onClick={() => router.replace("/")}>
-//         Home
-//       </button>
-//     </>
-//   );
-// }
-
-// export default Index;
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
-const UserData= ()=>{
-  const [users,setUsers] = useState([])
-  console.log(users);
-  useEffect(()=>{
-    async function fetchData (){
-      const data = await fetch("https://dummyjson.com/users");
-      setUsers(await data.json());
-    }
-    fetchData()
-  }, []);
-  return(
-    <div>
-    <h4>Users</h4>
-    {
-      users &&
-      users.users &&
-      users.users.map((user)=><Link href={`/user/${user.id}`} key={user.id}>
-        <div>
+import useSWR from "swr";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-        {user.firstName}
-        </div>
-        
-        </Link>)
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+const UserData = () => {
+    const { data, error } = useSWR("https://dummyjson.com/users", fetcher);
+    console.log(data);
+
+    if (error) {
+        return <h1>Error****</h1>;
     }
-    </div>
-  )
+    if (!data) {
+        return <h>Data Loading</h>;
+    }
+
+    return (
+        <div className="container mt-4">
+            <h4 className="mb-4">Users</h4>
+            
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.users && data.users.map((user, index) => (
+                        <tr key={user.id}>
+                            <td>{index + 1}</td>
+                            <td>
+                                <Link href={`/user/${user.id}`}>
+                                    {user.firstName}
+                                </Link>
+                            </td>
+                            <td>{user.lastName}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            
+        </div>
+    );
 }
-export default UserData
+
+export default UserData;
